5de00f361543d1d6dba801d5a26d7d900896127f
KrisztianCsillag <kcsillag@maxedy.com>
2021-10-15 21:24:22 +0200

    Hospital version 1

--- weboldal/src/App.js
+++ weboldal/src/App.js
@@ -1,80 +1,70 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import axios from "axios";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Patients from "./components/Patients";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HospitalLogo from "./images/kisspng-physician-medicine-computer-icons-hospital-health-cropped-icon-png-physician-senior-services-5d05236ae760a0.7278499315606178349477.png";
 
-function Valami(props) {
-  return <p>{props.valami}</p>
-}
+export default function App() {
+  const [database, setDatabase] = useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("http://localhost:8000/1");
+      setDatabase(result.data);
+    };
+    setTimeout(fetchData, 4000);
+  }, []);
 
-function App() {
-
-  const profile1 = {
-    name:'Csillag Krisztian',
-    paragraph:'Tanitok programozast',
-    image:'https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-1/p160x160/125482754_3875961809082824_6965142187480618367_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=blLGUDvoYz0AX-C2UmW&_nc_oc=AQlMbwcSd9-DTlCMtt1iTMxXJg0SD9B_B_sCjjJKNIuK3D8aOARXgJsRdNE3RVKbnlj4RbqWa7-KS6MuYYGdi4eU&_nc_ht=scontent-ams4-1.xx&oh=001a5eee5b1041aff476c57682bab969&oe=615FE3B0',
-    like:false,
+  if (!database) {
+    return (
+      <div className="heart-container">
+        <div className="lds-heart">
+          <div></div>
+        </div>
+      </div>
+    );
   }
-
-  const profile2 = {
-    name:'Blala',
-    paragraph:'Tanitok programozast',
-    image:'https://live.staticflickr.com/5056/5563677179_f16fc58afb_c.jpg',
-    like:false,
-  }
-
-  const initialDatabase = [
-    profile1,
-    profile2,
-    {
-      name:"Cica",
-      paragraph:'MIVA',
-      image:'',
-      like:false,
-    }
-  ]
-
-  // 1 state - adat, amely a pillanatnyi adatot jelkepezi
-  // 2 setState - funkcio, amelyel kicseljuk a pillanatnyi adatot
-  // 3 useState() - aminek az argumentuma a kezdo pillanatnyi adat
-
-  
-  const initialNumber = 0;
-        //1        2             3
-  const [number, setNumber] = useState(initialNumber);
-  const [database, setDatabase] = useState(initialDatabase);
-  const {name,paragraph,image,like} = database[number];
 
   return (
-    <div style={{
-      textAlign:'center'
-    }}>
-      {/** 
-       * <{definicio}> {tartalom} </{definicio}>
-       */}
-      <h1>{name}</h1>
-      <img src={image} alt={"profile"}/>
-      <p>{paragraph}</p>
-      <input type={'email'}></input>
-      <br/>
-      <Valami></Valami>
-      <button>SEND</button>
-      <br></br>
-      <br></br>
-      <button onClick={()=>{
-        setDatabase(database.map((profile)=>{
-          if(profile.name === name) {
-            profile.like = !like;
-          }
-          return profile;
-        }))
-      }}>{ like ? 'Unlike' : 'Like' }</button>
-      <br></br>
-      <button onClick={()=>{if(0 !== number)setNumber(number-1)}}>Prev</button>
-      <button onClick={()=>{if(database.length-1 !== number)setNumber(number+1)}}>Next</button>
-      
-      <p style={{marginTop:'128px'}}>Copyright is for Csillag Krisztian</p>
-      <button>Mennye az oldara</button>
+    <div className="App">
+      <Navbar
+        sticky="top"
+        style={{ background: "white" }}
+        className="border-bottom border-dark"
+      >
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt="Hospital Logo"
+              src={HospitalLogo}
+              width="60"
+              height="60"
+            />
+            {"Bácska Hospital Service"}
+          </Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/hospitals">Our hospitals</Nav.Link>
+            <Nav.Link href="/about"> About us</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/hospitals">
+            <Patients />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
---
+++ weboldal/src/components/Home/index.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../../images/kisspng-physician-medicine-computer-icons-hospital-health-cropped-icon-png-physician-senior-services-5d05236ae760a0.7278499315606178349477.png";
+import doctor from "../../images/doctor.png";
+import hospital from "../../images/hospital (1).png";
+import heart from "../../images/heart.png";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const lo = "Bácska Hospital Service";
+  const lo1 = "Welcome to...";
+  return (
+    <div>
+      <Container fluid="true" className="justify-content mt-4">
+        <Row>
+          <Col>
+            <h4 style={{ opacity: "0.4", marginLeft: "12vh" }}>{lo1}</h4>
+            <img
+              style={{ height: "200px", display: "block", margin: "auto" }}
+              src={logo}
+            ></img>
+            <h1 className="text-center font-weight-bold">{lo} </h1>
+          </Col>
+        </Row>
+        <Row>
+          <h3
+            style={{ opacity: "0.8", textAlign: "center", marginTop: "60px" }}
+          >
+            "How can we help today?"
+          </h3>
+          <img
+            style={{ width: "400px", margin: "auto", marginTop: "16px" }}
+            src={doctor}
+          ></img>
+        </Row>
+        <Row style={{ marginTop: "40px" }}>
+          <Col
+            style={{
+              marginTop: "40px",
+              marginLeft: "32px",
+              marginRight: "16px",
+              border: "solid 1px #e7f4f4",
+              borderRadius: "15%",
+              boxShadow: "0px 2px #e7f4f4",
+            }}
+          >
+            <Link to={"/hospitals"}>
+              <h2
+                style={{
+                  opacity: "0.6",
+                  textAlign: "center",
+                  marginTop: "16px",
+                }}
+              >
+                "Looking for a hospital."
+              </h2>
+              <img
+                style={{ display: "block", margin: "auto" }}
+                src={heart}
+              ></img>
+            </Link>
+          </Col>
+          <Col
+            style={{
+              marginTop: "40px",
+              marginLeft: "16px",
+              marginRight: "32px",
+              border: "solid 1px #e7f4f4",
+              borderRadius: "15%",
+              boxShadow: "0px 2px #e7f4f4",
+            }}
+          >
+            <Link to={"/about"}>
+              <h2
+                style={{
+                  opacity: "0.6",
+                  textAlign: "center",
+                  marginTop: "16px",
+                }}
+              >
+                "Tell me about your service."
+              </h2>
+              <img
+                style={{ display: "block", margin: "auto" }}
+                src={hospital}
+              ></img>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
