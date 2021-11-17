e2b59f24903d46e2ea517e532e5189755d99f6fa
Justb612 <borisz0929@gmail.com>
2021-11-17 20:55:11 +0100

    asd

--- weboldal/src/App.js
+++ weboldal/src/App.js
@@ -1,23 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { ReactComponent as Eye } from "./images/eye.svg";
-import { ReactComponent as Mouth } from "./images/mouth.svg";
-import { ReactComponent as Nose } from "./images/nose.svg";
-import { ReactComponent as Head } from "./images/head.svg";
-import { ReactComponent as Ear } from "./images/ear.svg";
-import { ReactComponent as Body } from "./images/body.svg";
-import { ReactComponent as Tooth } from "./images/tooth.svg";
 import axios from "axios";
 import "./App.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import Hospitals from "./components/Hospitals";
-import Hospital from "./components/Hospital";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import HospitalLogo from "./images/kisspng-physician-medicine-computer-icons-hospital-health-cropped-icon-png-physician-senior-services-5d05236ae760a0.7278499315606178349477.png";
-import HumanFactory from "./components/HumanFactory/index";
 
 export default function App() {
   const [database, setDatabase] = useState();
