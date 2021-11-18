b9ab85abda1f78638112aed06bf08cb0e2d30a7b
Justb612 <borisz0929@gmail.com>
2021-11-18 17:20:46 +0100

    asd

--- weboldal/src/App.js
+++ weboldal/src/App.js
@@ -31,7 +31,6 @@ export default function App() {
       </div>
     );
   }
-  console.log(database);
 
   return (
     <div>
--- weboldal/src/components/CsillagLikorHazi/Home.js
+++ weboldal/src/components/CsillagLikorHazi/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-export default function Home(database) {
+export default function Home() {
   return (
     <div>
       <Container>
         <Row>
-          <Col></Col>
+          <Col>1 of 3</Col>
           <Col xs={6}>2 of 3 (wider)</Col>
           <Col>3 of 3</Col>
         </Row>
