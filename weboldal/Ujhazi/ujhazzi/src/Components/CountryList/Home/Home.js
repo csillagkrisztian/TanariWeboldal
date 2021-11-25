4b56f07fe651f8c87d1262a2997be10225081979
Schloder <86109796+Schloder@users.noreply.github.com>
2021-11-25 06:13:29 +0100

    Fölrajzipofozgatások

--- weboldal/Ujhazi/ujhazzi/src/Components/CountryList/Home/Home.js
+++ weboldal/Ujhazi/ujhazzi/src/Components/CountryList/Home/Home.js
@@ -5,7 +5,7 @@ var ourTeacher = "Csillag Krisztina Porn Teacher Senpai";
 
 export default function Home() {
   return (
-    <div>
+    <div className="d1">
       <h1>{ourTeacher}</h1>
 
       <a className="a1" href="/countrylist">
@@ -13,15 +13,15 @@ export default function Home() {
           src="https://www.universetoday.com/wp-content/uploads/2010/03/Earth-Pacific-Ocean.jpg"
           className="firstImage"
         ></img>
-        <p>List of Countries</p>{" "}
+        <p className="p1">List of Countries</p>{" "}
       </a>
       <a className="b1" href="visitedcountries">
         <img src="https://thumbs.dreamstime.com/z/travel-to-abroad-enjoy-travelling-items-travelling-items-to-travel-abroad-137085176.jpg"></img>
-        <p>Countries I've visited</p>
+        <p className="p2">Countries I've visited</p>
       </a>
       <a className="c1" href="/teacher">
         <img src="https://lh3.googleusercontent.com/proxy/5YkSbfXkS1mR77989KRS4E2ghJIgXF7xgQRDykgwBd4rQdO97HKBeIoC5HCTtn0uvg8p3TjFqrG5t9J9IUiOrOnaIF8-NDHq1HTNW1lG5DWzVpxqzFPKzv0koNXj" />
-        <p>Szexi tanárnéni</p>
+        <p className="p3">Szexi tanárnéni</p>
       </a>
     </div>
   );
--- weboldal/Ujhazi/ujhazzi/src/Components/CountryList/Teacher/Teacher.js
+++ weboldal/Ujhazi/ujhazzi/src/Components/CountryList/Teacher/Teacher.js
@@ -1,5 +1,64 @@
 import React from "react";
+import { Card, Row, Col, Container, Button, CardGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Teacher.css";
+
+const szöveg =
+  "A gyerekeket arról kérdi a tanárnő, mivel foglalkozik a papájuk.";
+const szöveg2 = "- Az én apám ügyvéd! - mondja az első kisgyerek.";
+const szöveg3 = "- Az én apám orvos, embereket gyógyít! - mondja a második.";
+const szöveg4 = "- Az én apám meghalt - mondja Pistike.";
+const szöveg5 =
+  "- Ezt sajnálattal hallom - mondja a tanárnő. - És mit csinált, mielőtt meghalt volna?";
+const szöveg6 =
+  "- Először elvörösödött, aztán elkékült, majd összesett a szőnyegen...";
 
 export default function Teacher() {
-  return <div>LOlololol</div>;
+  return (
+    <div>
+      <Container className="fő">
+        <Row>
+          <Col className="nev">Csillag Krisztina</Col>
+        </Row>
+        <Row>
+          <Col>
+            <img
+              src="https://st4.depositphotos.com/3584053/20167/i/1600/depositphotos_201674940-stock-photo-confident-and-sexy-teacher-sensual.jpg"
+              className="kep"
+            />
+          </Col>
+          <Col className="szoveg">
+            {szöveg}
+            <br />
+            {szöveg2}
+            <br />
+            {szöveg3}
+            <br />
+            {szöveg4}
+            <br />
+            {szöveg5}
+            <br />
+            {szöveg6}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <a className="home" href="/">
+              Home
+            </a>
+          </Col>
+          <Col>
+            <Card className="card1">Spain</Card>
+            <Card className="card2">English</Card>
+            <Card className="card3">German</Card>
+          </Col>
+          <Col className="itt">
+            <a className="ott" href="visitedcountries">
+              Countries I've visited
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
